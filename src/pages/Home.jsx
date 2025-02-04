2070915b3e4b803ede64cf8f0dae843d47160de3
Frankam487 <fkamgang06@gmail.com>
2025-02-04 14:29:27 +0100

    link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ function Home() {
             </p>
             <motion.NavLink
               to="/services"
-              whileHover={{ scale: 1.05 }}
+              // whileHover={{ scale: 1 }}
               className="mt-6 inline-block bg-purple-600 text-white px-6 py-3 rounded-lg text-lg font-semibold shadow-md"
             >
               Découvrir nos services
