5d33eb4b661be2ff21db92fd45b3c216c1fac5bb
Frankam487 <fkamgang06@gmail.com>
2025-02-05 14:47:41 +0100

    images added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ContactPage from "./pages/ContactPage";
 import Services from "./components/Services";
 import ServiceDetails from "./components/ServiceDetails";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/services/:id" element={<ServiceDetails />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
