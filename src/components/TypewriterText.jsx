bcf31e4b6f53d3954d25cb7722c96ba552cb6004
Frankam487 <fkamgang06@gmail.com>
2025-02-06 17:08:35 +0100

    text typing

--- src/components/TypewriterText.jsx
+++ src/components/TypewriterText.jsx
@@ -7,16 +7,22 @@ const TypewriterText = () => {
   const [colorIndex, setColorIndex] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    const changeColor = () => {
       setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
-    }, 4000); 
-    return () => clearInterval(interval);
+    };
+    
+    const interval = setInterval(changeColor, 4000);
+
+    return () => clearInterval(interval); // Nettoyage de l'intervalle à la destruction du composant
   }, []);
 
   return (
     <div className="flex justify-center items-center min-h-[200px]">
       <div className="text-center">
-        <h1 className={`text-2xl md:text-6xl mb-4  transition-colors duration-500 ${colors[colorIndex]}`}>
+        <h1
+          className={`text-2xl md:text-6xl mb-4 transition-colors duration-500 ${colors[colorIndex]}`}
+          aria-live="polite" // Pour améliorer l'accessibilité
+        >
           <Typewriter
             options={{
               strings: [
